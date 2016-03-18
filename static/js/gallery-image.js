8d7fbae545ab5037061d8de47f1656ed37ea1ecc
liuasliy <liuasliy@live.com>
2016-03-18 10:27:56 +0800

    fix

--- static/js/gallery-image.js
+++
@@ -1,7 +0,0 @@
-(function($) { $(document).ready(function() { new grid3D(document.getElementById('grid3d'));
-        var container = document.querySelector('#gallery-container');
-        var msnry;
-        imagesLoaded(container, function() { msnry = new Masonry(container, { itemSelector: '.gallery-image', transitionDuration: '0.3s', columnWidth: container.querySelector('.gallery-image') }) }); }); })(jQuery);;
-(function($) { $(document).ready(function() { $('.toggle-menu').jPushMenu(); }); })(jQuery);
-
-
